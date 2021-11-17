bf455b1356291a736a329ae291050a5e775ad906
snehaabysusan <78409090+snehaabysusan@users.noreply.github.com>
2021-11-17 19:20:11 +0530

    Add files via upload

---
+++ ADV-C81/main.js
@@ -0,0 +1,42 @@
+
+canvas = document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+
+color = "red"; 
+
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 2;
+ctx.arc(500, 200, 40 ,0 , 2*Math.PI);
+ctx.stroke();
+
+canvas.addEventListener("mousedown", my_mousedown);
+
+function my_mousedown(e)
+{
+    
+    
+     mouse_x = e.clientX - canvas.offsetLeft;
+     mouse_y = e.clientY - canvas.offsetTop;
+
+    
+    circle(mouse_x , mouse_y);    
+}
+
+function circle(mouse_x , mouse_y)
+{
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 2;
+ctx.arc(mouse_x, mouse_y, 40 ,0 , 2*Math.PI);
+ctx.stroke();
+}
+
+//additional activity
+
+function clearArea()
+{
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+	
